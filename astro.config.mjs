b47ae32fc220397e0848fb6959d2ccde96a108cc
GonzDv <xliqn0@gmail.com>
2026-04-15 17:29:57 -0600

    feat: improve SEO page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://www.knarq.com',
 	output: 'static',
 	adapter: vercel(),
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	integrations: [sitemap()],
 });
