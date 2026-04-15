119fdf222e9b4e50dac4d822cef33e61eac35ed3
GonzDv <xliqn0@gmail.com>
2026-04-14 20:08:33 -0600

    feat: change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', 
-  adapter: vercel(),
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	output: 'static',
+	adapter: vercel(),
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
