670b3e0d04be730925102c3cb6184b51c2270db9
GonzDv <xliqn0@gmail.com>
2026-04-15 10:23:49 -0600

    feat: add number to contact mail

--- src/pages/api/contacto.ts
+++ src/pages/api/contacto.ts
@@ -8,11 +8,12 @@ export const POST: APIRoute = async ({ request }) => {
   // 1. Recibimos los datos del formulario frontend
   const data = await request.formData();
   const name = data.get("name") as string;
+  const phone = data.get("phone") as string; 
   const email = data.get("email") as string;
   const message = data.get("message") as string;
 
   // 2. Validación básica de seguridad
-  if (!name || !email || !message) {
+  if (!name || !email || !message || !phone) {
     return new Response(
       JSON.stringify({ error: "Faltan campos obligatorios" }),
       { status: 400 },
@@ -28,6 +29,7 @@ export const POST: APIRoute = async ({ request }) => {
       html: `
         <h2>Nuevo contacto desde la página web</h2>
         <p><strong>Nombre:</strong> ${name}</p>
+        <p><strong>Telefono:</strong> ${phone}</p>
         <p><strong>Correo:</strong> ${email}</p>
         <p><strong>Mensaje:</strong><br/> ${message}</p>
       `,
